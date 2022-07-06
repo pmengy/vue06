d621bb223d903b5ab70ac2b56cab2376a1e0d7ab
pmengy <936841379@qq.com>
2022-07-06 21:06:51 +0800

    完成表格数据渲染

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,23 @@
 <template>
   <div>
-    <van-tabs
-      @click="onClick"
-      background="#07c160"
-      title-active-color="pink"
-      title-inactive-color="white"
-      font-size="large"
-    >
-      <van-tab title="手机注册">
-        <TelLogin> </TelLogin>
-      </van-tab>
-      <van-tab title="邮箱注册">
-        <EmailLogin></EmailLogin>
-      </van-tab>
-    </van-tabs>
+    <MyTable>
+      
+    </MyTable>
   </div>
 </template>
 
 <script>
-import TelLogin from './components/TeleLogin.vue';
-import EmailLogin from './components/EmailLogin.vue';
-
+import MyTable from './components/MyTable.vue';
 export default {
-  methods: {
-    onClick(name, title) {
-      console.log(name, title);
-    },
+  data() {
+    return {};
   },
+
+  mounted() {},
   components: {
-    TelLogin,
-    EmailLogin,
+    MyTable,
   },
+
+  methods: {},
 };
 </script>
-
-<style lang="less" scoped></style>
